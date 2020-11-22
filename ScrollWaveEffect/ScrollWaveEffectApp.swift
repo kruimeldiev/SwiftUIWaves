32a6f8422c2d3c96b9ae61ec7e7eb69ad7997b6a
Casper Daris <casperdaris@msn.com>
2020-11-22 14:00:19 +0100

    Initial Commit

---
+++ ScrollWaveEffect/ScrollWaveEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollWaveEffectApp.swift
+//  ScrollWaveEffect
+//
+//  Created by Casper Daris on 22/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct ScrollWaveEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
